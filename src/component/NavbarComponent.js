96f7a225a5868701bac9701973afbcc62fd7757f
zainahda <zainahda@gmail.com>
2021-11-12 14:56:29 +0700

    Setting layout navbar

--- src/component/NavbarComponent.js
+++ src/component/NavbarComponent.js
@@ -4,9 +4,9 @@ import {Navbar, Container} from 'react-bootstrap'
 const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark">
-    <Container>
+    <Container className="d-flex justify-content-center">
       <Navbar.Brand href="#home">
-      Arkana Cashier App
+      <strong>Arkana</strong> Cashier App
       </Navbar.Brand>
     </Container>
   </Navbar>
