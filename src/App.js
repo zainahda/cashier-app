cce70e2fa60f80343fae4f38b190181cf5db5f55
zainahda <zainahda@gmail.com>
2021-11-12 23:29:50 +0700

    Setting layout cashier app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import NavbarComponent from './component/NavbarComponent';
+import { Col, Row, Container } from "react-bootstrap";
+import "./App.css";
+import { Hasil, ListCategories, NavbarComponent } from "./components/Index";
+
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-2">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/ListCategories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Col } from 'react-bootstrap'
+
+
+export default class ListCategories extends Component {
+  render() {
+    return (
+      <Col md={2}>
+          <h4><strong>Daftar Kategori</strong></h4>
+          <hr />
+      </Col>
+    )
+  }
+}
